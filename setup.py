4dd8e386b7243e8a9ef28fb98b518f8d777786bc
xpxqx <xpxqx@users.noreply.github.com>
2014-04-06 11:49:30 +0100

    load module meta automatically during setup

--- setup.py
+++ setup.py
@@ -1,24 +1,43 @@
+#!/usr/bin/env python3
 """
 Pymatrix: a lightweight matrix module in pure Python with support for 
 basic linear algebra operations.
 
 """
 
+import os
+import re
+import io
+
 from distutils.core import setup
+
+
+def load_meta(*path):
+    filepath = os.path.join(os.path.dirname(__file__), *path)
+    with io.open(filepath, encoding='utf-8') as mfile:
+        pystr = mfile.read()
+        regex = r'''^__([a-z]+)__ = "(.*)"'''
+        return dict(re.findall(regex, pystr, flags=re.MULTILINE))
+
+
+meta = load_meta('pymatrix.py')
+
 
 short_description = (
     'A lightweight matrix object with support for '
     'basic linear algebra operations.'
 )
 
+
 long_description = """\
 See the module's Github homepage (https://github.com/xpxqx/pymatrix) 
 for further details.
 """
 
+
 setup(
     name = 'pymatrix',
-    version = '1.0.1',
+    version = meta['version'],
     url = 'https://github.com/xpxqx/pymatrix',
     license = 'Public Domain',
     author = 'xpxqx',
